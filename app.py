0e431ce053da87180e985180de9bf6df409a1415
JohnnyLearnning <145878236+JohnnyLearnning@users.noreply.github.com>
2024-03-05 07:05:49 -0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# Dont name your Flask directory as flask by itself
+# Make a python environment and install Flask
+# python -m venv .venv
+# Do not push the .venv folder to git
+# use the .gitingnore file
+# activate the environment
+# .venv\Scripts\activate or go into scripts and run activate
+# log into git and create repository
+# If you running into problems runing python choco install -y python
+# Initialize the git repository
+#  git init
+#  git add .
+# git commit -m "Initial Commit"
+# git branch -M main
+# git remote add origin 
+# git push -u origin main
+
+
+# pip install flask
